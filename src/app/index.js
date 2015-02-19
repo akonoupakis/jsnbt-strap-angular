c61d467d4be5e94aa9ea24c1a24349462275f122
Apostolis Konoupakis <konoupakis@gmail.com>
2015-02-19 02:24:49 +0200

    index.route(), index.view.prerender(), index.view.render()

--- src/app/index.js
+++ src/app/index.js
@@ -6,6 +6,24 @@ module.exports = {
 
     getConfig: function () {
         return require('./config.js');
+    },
+
+    route: function (ctx, next) {
+        // intercept the routing process here, or trigger the next router        
+
+        next();
+    },
+
+    view: {
+
+        prerender: function (ctx) {
+            // change here the ctx.model and the ctx.tmpl before rendering
+        },
+
+        render: function (ctx) {
+            // change here the ctx.html upon render
+        }
+
     }
 
 };
