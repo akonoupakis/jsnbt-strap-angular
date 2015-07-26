60ec7bc1590ebcaa74b810ad8994527f4cd9e34a
Apostolis Konoupakis <konoupakis@gmail.com>
2015-07-26 13:48:56 +0300

    jsnbt updates

--- src/cfg/config.js
+++ src/cfg/config.js
@@ -35,8 +35,8 @@ module.exports = {
     scripts: [{
         name: 'lib',
         items: [
-            '/js/lib/angular.js',
             '/js/lib/jquery.js',
+            '/js/lib/angular.js',
             '/js/lib/underscore.js',
             '/js/lib/underscore.string.js'
         ]
@@ -46,9 +46,6 @@ module.exports = {
             '/jsnbt.js',
             '/js/app/main.js',
             '/js/app/controllers/AppController.js',
-            '/js/app/controllers/StoreController.js',
-            '/js/app/controllers/StoresController.js',
-            '/js/app/directives/ctrlMap.js',
             '/js/app/providers/$context.js',
             '/js/app/providers/$data.js',
             '/js/app/providers/$image.js',
@@ -62,7 +59,7 @@ module.exports = {
     templates: [{
         id: 'error',
         name: 'error page',
-        html: '/error/error.html',
+        html: '/err/error.html',
         restricted: [''],
         scripts: [
             ['lib', 'app']
